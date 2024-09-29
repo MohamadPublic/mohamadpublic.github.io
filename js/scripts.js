1b186be39ea8e60676975820dbd417783799dc8c
Mohamad Ahmad <mohamad@bluegem.ai>
2024-09-29 14:37:38 -0400

    Add files via upload
    Initial commit.

---
+++ js/scripts.js
@@ -0,0 +1,116 @@
+/*!
+ * Start Bootstrap - Resume v7.0.2 (https://startbootstrap.com/theme/resume)
+ * Copyright 2013-2021 Start Bootstrap
+ * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
+ */
+//
+// Scripts
+//
+
+// Force page to scroll to top on page load
+if (history.scrollRestoration) {
+  history.scrollRestoration = "manual";
+} else {
+  window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
+  };
+}
+
+window.addEventListener("DOMContentLoaded", (event) => {
+  // Collapse responsive navbar when toggler is visible
+  const navbarToggler = document.body.querySelector(".navbar-toggler");
+  const responsiveNavItems = [].slice.call(
+    document.querySelectorAll("#navbarResponsive .nav-link")
+  );
+  responsiveNavItems.map(function (responsiveNavItem) {
+    responsiveNavItem.addEventListener("click", () => {
+      if (window.getComputedStyle(navbarToggler).display !== "none") {
+        navbarToggler.click();
+      }
+    });
+  });
+});
+
+$(document).ready(function () {
+  // JQuery ease-in effect for the navbar
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+    if (
+      location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
+      location.hostname == this.hostname
+    ) {
+      var target = $(this.hash);
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+      if (target.length) {
+        $("html, body").animate(
+          {
+            scrollTop: target.offset().top,
+          },
+          1000,
+          "easeInOutQuart"
+        );
+        return false;
+      }
+    }
+  });
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $(".js-scroll-trigger").click(function () {
+    $(".navbar-collapse").collapse("hide");
+  });
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $("body").scrollspy({
+    target: "#sideNav",
+    offset: 80,
+  });
+
+  $("#submit-button").click(function (evt) {
+    let name = $("#name").val();
+    let email = $("#email").val();
+    let message = $("#message").val();
+
+    var emailStatus = $("#email-status");
+    emailStatus.empty();
+
+    var nameStatus = $("#name-status");
+    nameStatus.empty();
+
+    var messageStatus = $("#message-status");
+    messageStatus.empty();
+
+    if (!validateName(name)) {
+      evt.preventDefault();
+      nameStatus.append("<p>Name field required<p>");
+    }
+
+    if (!validateEmail(email)) {
+      evt.preventDefault();
+      if (email.length === 0) {
+        emailStatus.append("<p>Email field required<p>");
+      } else {
+        emailStatus.append("<p>Invalid email</p>");
+      }
+    }
+
+    if (!validateMessage(message)) {
+      evt.preventDefault();
+      messageStatus.append("<p>Message field required<p>");
+    }
+  });
+});
+
+validateEmail = function (email) {
+  // Check if the email is valid
+  var re =
+    /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+  return re.test(email);
+};
+
+validateName = function (name) {
+  return name.length > 0;
+};
+
+validateMessage = function (message) {
+  return message.length > 0;
+};
